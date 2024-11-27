591e2e43fbcb93998fc584037365635f20dd4460
Rayan <139246019+Rayanzin@users.noreply.github.com>
2024-11-27 18:55:13 -0300

    UP

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function listarPokemons(){
+    const url = `https://pokeapi.co/api/v2/pokemon/25`
+    fetch(url).then(response => response.json()).then(pokemon =>{
+        console.log(pokemon);
+        
+    })
+}
+listarPokemons()
